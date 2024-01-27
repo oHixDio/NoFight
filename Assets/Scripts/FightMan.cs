dc468c5d750e0b7c5e8da64d06314c1e72203ee3
oHixDio <hry66H@outlook.jp>
2024-01-27 22:17:06 +0900

    FightMan吹っ飛ぶ カメラつけた

---
+++ Assets/Scripts/FightMan.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightMan : MonoBehaviour
+{
+    enum EDir { L,R,}
+
+    [SerializeField]
+    private EDir dir = EDir.L;
+
+    [SerializeField]
+    private float power = 1.0f;
+
+    private bool isMove;
+
+    private IGameState gameState;
+
+
+
+    void Start()
+    {
+        gameState = GameManager.instace.GetComponent<IGameState>();
+    }
+
+    void Update()
+    {
+        if (gameState.GetCurrentGameState() == EGameState.FLY)
+        {
+            isMove = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isMove)
+        {
+            // relativePosition‚ÌˆÚ“®
+            transform.Translate(0, 0, 1 * -power * Time.deltaTime);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        if (gameState.GetCurrentGameState() != EGameState.CUTIN)
+        {
+            gameState.ChangeGameState(EGameState.CUTIN);
+        }
+        
+    }
+}
