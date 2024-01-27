7c59640905f14ebf2933e1a0f08c949f012b296f
rapio2525 <sba22g030001@edu.sba.ac.jp>
2024-01-27 18:57:23 +0900

    bagu修正

--- Assets/Scripts/addforce.cs
+++ Assets/Scripts/addforce.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
+//<<<<<<< HEAD
 
 
 [RequireComponent(typeof(Rigidbody))]
@@ -31,26 +31,4 @@ public class addforce : MonoBehaviour
     }
 }
 
-=======
-using UnityEngine.UI;
 
-public class addforce : MonoBehaviour
-{
-
-    public GameObject Human;
-    void FixedUpdate()
-    {
-        Transform transform = this.GetComponent<Transform>(); //Transform‚ğæ“¾
-        Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbody‚ğæ“¾
-        Vector3 force = new Vector3(1,0,0);    // —Í‚ğİ’è
-        rb.AddForce(force, ForceMode.Force)  ;
-
-        if (Human.transform.position.z <= 500)
-        {
-            Debug.Log("¬Œ÷");
-        }
-    }
-
-    
-}
->>>>>>> 43cbe87a1da176080bdb591826c0eb9f9ed61044
