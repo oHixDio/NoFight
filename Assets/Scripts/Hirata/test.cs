6852f20577a12d813be75aaaa8f69e4faf7d1a8c
DESKTOP-5N09ND1\ggj2024 <SBA22G030007@edu.sba.ac.jp>
2024-01-27 17:53:40 +0900

    音声データ追加、再生の処理も作成

--- Assets/Scripts/Hirata/test.cs
+++ Assets/Scripts/Hirata/test.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class test : MonoBehaviour
 {
-    //SetActive
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("ƒeƒXƒg");
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.M))
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+        }
     }
 }
