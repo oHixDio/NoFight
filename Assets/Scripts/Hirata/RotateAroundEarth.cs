47dff0ed4fe72eb231335f5bef6db37c3ae80dbd
DESKTOP-5N09ND1\ggj2024 <SBA22G030007@edu.sba.ac.jp>
2024-01-27 22:54:16 +0900

    宇宙を作った

--- Assets/Scripts/Hirata/RotateAroundEarth.cs
+++ Assets/Scripts/Hirata/RotateAroundEarth.cs
@@ -7,12 +7,12 @@ public class RotateAroundEarth : MonoBehaviour
     //一定の地点まで回転するとカメラが切り替わり男たちが握手する
     //回転移動する男にアタッチする
 
-    //中心点
-    [SerializeField] private Vector3 center = Vector3.zero;
-    //回転軸
-    [SerializeField] private Vector3 axis = Vector3.up;
-    //円運動周期
-    [SerializeField] private float _period = 2;
+    // 中心点
+    [SerializeField] private Vector3 _center = Vector3.zero;
+    // 回転軸
+    [SerializeField] private Vector3 _axis = Vector3.forward;
+    // 円運動周期
+    [SerializeField] private float _period = 6;
 
     //回転の停止する地点
     private float stopRotatePos_Max = -5.4f;
@@ -22,39 +22,72 @@ public class RotateAroundEarth : MonoBehaviour
     [SerializeField] private GameObject explosion;
     private bool isexplosion = false;
 
+    //インスペクター上でSEを設定
+    public AudioSource audioSource;
+    public AudioClip SE;
+    public AudioClip BlowingSE;
+
     //吹き飛びレベルのスクリプタブルオブジェクト
     public BlowingLevel BlowingLevel;
 
     private void Update()
     {
-        
-        
-
-        //スライダーが強かつ一定の地点まで回転すると動作
-        if (transform.position.y < stopRotatePos_Medium && BlowingLevel.level == BlowingLevel.Level.Medium)
+        //他のスクリプトからフラグをtrueにすると動作
+        if (BlowingLevel.isCosmicShift)
         {
+            RotateAroundEarthGays();
+        }
+    }
+
+    //根幹の処理をメソッドにして管理しやすくする
+    public void RotateAroundEarthGays()
+    {
+        //スライダーが強かつ一定の地点まで回転すると動作
+        if (transform.position.y < stopRotatePos_Medium && BlowingLevel.level == BlowingLevel.Level.Good)
+        {
+            //audioのループ設定を元に戻す
+            AudioLoopReverse();
+
             //宇宙でのイベントが起きていない時発生させる
             if (!BlowingLevel.CosmicFall_flag)
             {
-                
-
+                //trueにして爆発させる
                 BlowingLevel.CosmicFall_flag = true;
             }
         }
         //スライダーが最大かつ一定の地点まで回転すると動作
-        else if (transform.position.y < stopRotatePos_Max && BlowingLevel.level == BlowingLevel.Level.High)
+        else if (transform.position.y < stopRotatePos_Max && BlowingLevel.level == BlowingLevel.Level.Great)
         {
+            //audioのループ設定を元に戻す
+            AudioLoopReverse();
+
             //フラグを変えてカメラを切り替える
             if (!BlowingLevel.CosmicFall_flag) BlowingLevel.CosmicFall_flag = true;
         }
         else
         {
-            //中心点centerの周りを、軸axisで、period周期で円運動
-            transform.RotateAround(
-                center,
-                axis,
-                360 / _period * Time.deltaTime
-            );
+            //回転を行わずに円周移動を行う処理
+            var tr = transform;
+            // 回転のクォータニオン作成
+            var angleAxis = Quaternion.AngleAxis(360 / _period * Time.deltaTime, _axis);
+
+            // 円運動の位置計算
+            var pos = tr.position;
+
+            pos -= _center;
+            pos = angleAxis * pos;
+            pos += _center;
+
+            tr.position = pos;
+
+
+            if (audioSource.loop == false)
+            {
+                //吹き飛ぶ時のSEを再生　この時audioをループ設定に変える
+                audioSource.loop = true;
+                //SEを再生
+                audioSource.PlayOneShot(BlowingSE);
+            }
         }
 
         //爆発するか確認
@@ -64,15 +97,33 @@ public class RotateAroundEarth : MonoBehaviour
     //一度だけ爆発を発生させる
     public void explosionCosmic()
     {
+        //宇宙でのフラグがtrueになると動作
         if (BlowingLevel.CosmicFall_flag)
         {
             if (!isexplosion)
             {
                 //爆発のオブジェクトを生成
                 Instantiate(explosion, transform.position, Quaternion.identity);
+                //SEを再生
+                audioSource.PlayOneShot(SE);
+
+                //人を非表示にする
+                gameObject.SetActive(false);
+
                 //trueにして再度発生しないようにする
                 isexplosion = true;
+
+
+                //数秒後次の場面に切り替える
+
             }
         }
+    }
+
+    //audioをループ設定を戻す
+    private void AudioLoopReverse()
+    {
+        //audioをループ設定がtrueの時falseにする
+        if (audioSource.loop == true) audioSource.loop = false;
     }
 }
