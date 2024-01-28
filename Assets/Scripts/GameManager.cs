7a73f5038811e3e8cce7641439d6ed007d09fe94
MSI-07\student <sba22g030004@edu.sba.ac.jp>
2024-01-28 13:41:21 +0900

    バグ修正

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -92,7 +92,7 @@ public class GameManager : MonoBehaviour, IGameState
         switch (curState)
         {
             case EGameState.TITLE:
-                canvas.SetActiveCanvas(true, state);
+                canvas.SetActiveCanvas(false, state);
                 break;
             case EGameState.START:
                 break;
